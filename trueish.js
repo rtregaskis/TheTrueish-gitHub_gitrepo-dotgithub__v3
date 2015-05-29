ddd60911773deae7e2aaf16cfeb5310a5094372b
rtregaskis <rtregaskis@dresources.com>
2015-05-29 15:02:50 +0100

    the commit that is the first commit

---
+++ trueish.js
@@ -0,0 +1,33 @@
+/**
+ * TODO: complete this
+ */
+function IsThisTrueFunction_1(paramThatMayBetrue)
+{
+ istHisTrue = paramThatMayBetrue
+   
+   switch (istHisTrue){
+   	case 'TRUE':
+		if (paramThatMayBetrue == 'TRUE')
+		{
+			if (typeof(istHisTrue) != 'boolean')
+			{
+				istHisTrue = true;
+				break;
+			}
+		}
+		case '0':
+			return 'no';
+		default:
+		break;
+		}
+
+
+   return true;
+    
+}
+
+
+function moreOrless_thantrueish(){
+istHisTrue;
+
+   }
